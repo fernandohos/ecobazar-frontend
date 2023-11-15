44b611a8028faaa90d7384dfa1da3213df3d2243
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2023-11-14 22:50:12 -0300

    :construction: add nav bar component

---
+++ src/components/nav-bar.tsx
@@ -0,0 +1,20 @@
+import { NavLink } from "./nav-link";
+
+const navItems = [
+  { title: "Home", url: "/" },
+  { title: "Shop", url: "/shop" },
+  { title: "About Us", url: "/about-us" },
+  { title: "Contact Us", url: "/contact" },
+];
+
+export function NavBar() {
+  return (
+    <ul className="flex flex-1 list-none">
+      {navItems.map(({ title, url }) => (
+        <NavLink key={url} href={url}>
+          {title}
+        </NavLink>
+      ))}
+    </ul>
+  );
+}
