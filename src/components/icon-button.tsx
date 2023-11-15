22d2eba423157bc66f73229f67fd3d41ddc24638
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2023-11-15 18:56:58 -0300

    :construction: add children to icon button component

--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -4,13 +4,14 @@ type IconButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   icon: any;
 };
 
-export function IconButton({ icon: Icon, ...props }: IconButtonProps) {
+export function IconButton({ icon: Icon, children, ...props }: IconButtonProps) {
   return (
     <button
-      className="rounded-full bg-white focus:brightness-95 hover:brightness-95 p-2 ease-out duration-200"
+      className="flex items-center gap-sm rounded-full bg-white focus:brightness-95 hover:brightness-95 p-2 ease-out duration-200"
       {...props}
     >
       <Icon className="w-[3rem] h-[3rem] ease-out duration-200 stroke-gray-900 hover:stroke-primary focus:stroke-primary" />
+      {children}
     </button>
   );
 }
