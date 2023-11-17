9e7b4d2b455598c7afcb8f7d8024bf0dde66d44c
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2023-11-16 22:49:38 -0300

    :construction: add hero background

---
+++ src/components/hero-background.tsx
@@ -0,0 +1,52 @@
+import Icon1 from "@public/hero-icons/icon-1.svg";
+import Icon2 from "@public/hero-icons/icon-2.svg";
+import Icon3 from "@public/hero-icons/icon-3.svg";
+import Icon4 from "@public/hero-icons/icon-4.svg";
+import Icon5 from "@public/hero-icons/icon-5.svg";
+import image1 from "@public/hero-icons/image-1.png";
+import image2 from "@public/hero-icons/image-2.png";
+import image3 from "@public/hero-icons/image-3.png";
+import image4 from "@public/hero-icons/image-4.png";
+import Image from "next/image";
+
+export function HeroBg() {
+  return (
+    <div className="absolute top-0 left-0 w-full h-full z-[1]">
+      <div className="relative w-full h-full">
+        <Icon1 className="absolute w-[10rem] bottom-0 right-0" />
+        <Icon2 className="absolute w-[10rem] top-[8%] right-[10vw]" />
+        <Icon3 className="absolute w-[12rem] top-[15%] left-[4vw]" />
+        <Icon4 className="absolute w-[8rem] top-0 right-[40vw]" />
+        <Icon5 className="absolute w-[13rem] bottom-[4%] left-[3vw]" />
+        <Image
+          src={image1}
+          width={80}
+          height={190}
+          alt="green leaf"
+          className="absolute top-0 right-0"
+        />
+        <Image
+          src={image2}
+          width={76}
+          height={86}
+          alt="green leaf"
+          className="absolute top-0 left-[3vw]"
+        />
+        <Image
+          src={image3}
+          width={39}
+          height={34}
+          alt="green leaf"
+          className="absolute bottom-[5%] left-[20vw]"
+        />
+        <Image
+          src={image4}
+          width={65}
+          height={106}
+          alt="green leaf"
+          className="absolute bottom-[-4rem] right-[20vw]"
+        />
+      </div>
+    </div>
+  );
+}
