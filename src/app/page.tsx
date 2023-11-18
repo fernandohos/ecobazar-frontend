c07983730da4642308d48f1b9a5d0887582e592f
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2023-11-18 18:22:15 -0300

    :sparkles: add feature section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { HomeHeader } from "@components/home-header";
 import { Hero } from "@components/hero";
+import { Feature } from "@components/feature-section";
 
 export default function Home() {
   return (
     <div>
       <HomeHeader />
       <Hero />
+      <Feature />
     </div>
   );
 }
---
+++ src/components/feature-section.tsx
@@ -0,0 +1,42 @@
+import { FeatureCard } from "./feature-card";
+import shippingIcon from "@public/shipping-icon.png";
+import supportIcon from "@public/support-icon.png";
+import paymentIcon from "@public/payment-icon.png";
+import boxIcon from "@public/box-icon.png";
+
+const featureCardsData = [
+  {
+    icon: shippingIcon,
+    title: "Free Shipping",
+    description: "Free shipping with discount",
+  },
+  {
+    icon: supportIcon,
+    title: "Great Support 24/7",
+    description: "Instant access to Contact",
+  },
+  {
+    icon: paymentIcon,
+    title: "100% Secure Payment",
+    description: "We ensure your money is save",
+    highlight: true,
+  },
+  {
+    icon: boxIcon,
+    title: "Money-Back Guarantee",
+    description: "30 days money-back",
+  },
+];
+
+export function Feature() {
+  return (
+    <section className="w-[90vw] m-auto mb-2xl">
+      <div className="grid grid-cols-2 min-[950px]:grid-cols-4 items-center">
+        {featureCardsData.map((cardData) => (
+          <FeatureCard key={cardData.title} {...cardData} />
+        ))}
+      </div>
+      <span className="block w-full h-[.2rem] bg-gray-100"></span>
+    </section>
+  );
+}
