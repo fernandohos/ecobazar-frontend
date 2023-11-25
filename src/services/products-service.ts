4593d4b973a79b550e4fe67f3e206aa8ef91b506
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2023-11-25 17:06:03 -0300

    :wrench: add NEXT_PUBLIC_ prefix to server url

--- src/services/products-service.ts
+++ src/services/products-service.ts
@@ -3,7 +3,7 @@ import { Product } from "@/types/Product";
 export async function getFeaturedProducts(): Promise<Product[]> {
   try {
     const { products } = await fetch(
-      `${process.env.SERVER_URL}/products?page_size=4`,
+      `${process.env.NEXT_PUBLIC_SERVER_URL}/products?page_size=4`,
       { method: "GET" }
     ).then((response) => response.json());
     return products;
